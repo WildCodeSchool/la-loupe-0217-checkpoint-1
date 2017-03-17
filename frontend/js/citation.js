abcd0e28dfb8d2285f848f18746b7d7c8f86bb90
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-03-17 13:17:34 +0100

    Cleaned code

--- frontend/js/citation.js
+++ frontend/js/citation.js
@@ -1,31 +1,3 @@
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", "../../backend/db.json", true);
-// xhr.send();
-//
-// xhr.onreadystatechange = getArticles;
-
-
-// function getArticles() {
-//   if (xhr.readyState == 4) {
-//     if (xhr.status == 200);
-//     var response = JSON.parse(xhr.responseText);
-//
-//     // var author = response.articles[0].author;
-//     // var chapter = response.articles[0].chapter;
-//     // var episode = response.articles[0].episode;
-//     // var content = response.articles[0].content;
-//     // var image = response.articles[0].image;
-//
-//     document.getElementById("quote").innerHTML = "<blockquote>" + content + "</blockquote>";
-//     document.getElementById("source").innerHTML = author + "<i>" + chapter + ", " + episode + "</i>";
-//     var sources = author + "<i>" + chapter + ", " + episode + "</i>";
-//     document.getElementById("img-box").innerHTML = '<img class="kaamelott-underline" src="' + image + '" alt="' + sources + '">';
-//   }
-// }
-
-
-
-
 function getArticles() {
     const xhr = new XMLHttpRequest();
     // citation = document.getElementById('thumbnail').value;
