eedf453cf144621dff3e8b52d44d8291f204889f
Sabrina <sabrina.mardjoeki@gmail.com>
2017-03-17 13:16:35 +0100

    first update

---
+++ frontend/perso.js
@@ -0,0 +1,30 @@
+function getArticles() {
+
+  var ville = document.getElementById('blockquote').value;
+
+
+	const req = new XMLHttpRequest();
+  req.onload = reqListener;
+req.open('get', 'http://localhost:3000/articles', true);
+req.send();
+
+
+function reqListener() {
+
+    var resultat = JSON.parse(this.responseText);
+    var author = resultat.author;
+    var chapter = resultat.chapter;
+    var episode = resultat.episode;
+    var content = resultat.content;
+    var date = resultat.date;
+    var image = resultat.images;
+
+    document.getElementByClassName("author").innerHTML = author;
+    document.getElementByClassName("chapter").innerHTML = chapter;
+    document.getElementByClassName("episode").innerHTML = episode;
+    document.getElementByClassName("content").innerHTML = content;
+    document.getElementByClassName("date").innerHTML = date;
+    document.getElementByClassName("images").innerHTML = images;
+
+}
+}
