45713ac97e1d6671bf37240b8f8342d0e97c1869
Rabin Julien <julienr114@gmail.com>
2017-03-20 17:37:58 +0100

    add quotes

---
+++ frontend/js/new.js
@@ -0,0 +1,16 @@
+const createArticles = function () {
+
+  var newQuote = {
+    author: document.getElementById('author').value,
+    chapter: document.getElementById('saison').value,
+    content: document.getElementById('quote').value,
+    image: document.getElementById('img').value,
+    episode: document.getElementById('episod').value
+  };
+
+  var req = new XMLHttpRequest
+  req.open('POST', 'http://localhost:3000/articles');
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send(JSON.stringify(newQuote));
+  window.location.href = 'index.html';
+}
